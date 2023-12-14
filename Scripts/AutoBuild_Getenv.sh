60a1a16569bf2ddafd5fb7e06e7e572fda9f1b39
erelease <105707344+erelease@users.noreply.github.com>
2023-12-15 02:27:08 +0800

    fix error

--- Scripts/AutoBuild_Getenv.sh
+++ Scripts/AutoBuild_Getenv.sh
@@ -11,8 +11,19 @@ echo "---------------------------------- Disk Info -----------------------------
 lsblk
 echo "------------------------------- Disk Usage Info ------------------------------"
 df -h
+
+
+# Get IP Info
 echo "------------------------------- IP Address Info ------------------------------"
 IP=`curl ip.115115.xyz -s`
+curl ip.115115.xyz -s
 curl -s https://searchplugin.csdn.net/api/v1/ip/get?ip=${IP} | jq -r .data.address
+}
+
+Get_Release_Info(){
+echo "------------------------------- Build Version Data ----------------------------"
 echo ${NOW_DATA_VERSION}
+echo "------------------------------ Build Kernel Version ---------------------------"
+`cat ${OPENWRT_BUILD_DIR}/target/linux/x86/Makefile | grep KERNEL_PATCHVER:=`
+
 }
